8342f6c123ec79c3ff47d01158385bca1129c211
axispx <anarchyrucks@live.com>
2024-12-13 01:27:41 -0500

    Get price from our own database

--- api/api.go
+++ api/api.go
@@ -3,7 +3,9 @@ package api
 import (
 	"net/http"
 
+	"github.com/antitokens/priceindex/model"
 	"github.com/antitokens/priceindex/source"
+	"github.com/antitokens/priceindex/utils"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -11,10 +13,20 @@ import (
 func GetPriceHandler(source source.Source, db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		token := c.Params("token")
-		priceResponse, err := source.GetPrice(token)
-		if err != nil {
-			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		address := utils.GetTokenAddress(token)
+
+		var price model.Price
+		if err := db.Where("address = ?", address).Order("timestamp DESC").First(&price).Error; err != nil {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Price not found"})
 		}
+
+		priceResponse := model.PriceResponse{
+			Price:     price.Price,
+			Timestamp: price.Timestamp.Unix(),
+			Source:    price.Source,
+			Address:   price.Address,
+		}
+
 		return c.JSON(priceResponse)
 	}
 }
